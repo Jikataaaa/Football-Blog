a59f1d35d8c692e42a1702cd9146132edc7eebaf
Zhivko Radev <jivkokt2003@abv.bg>
2021-12-06 23:55:03 +0200

    some work on static resources and on home and user controllers (test commit)

---
+++ src/main/java/springadvanced/exam/ExamApplication.java
@@ -0,0 +1,14 @@
+package springadvanced.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamApplication.class, args);
+    }
+
+}
